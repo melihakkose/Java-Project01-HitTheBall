3dfd85cbd4ed868737741a41fd60aa34d991ccb3
Melih <Olymposrec@users.noreply.github.com>
2020-08-12 16:40:19 +0300

    Android Studio ile Java Project01 "HitTheBall"
    Öğrendiklerimizi üzerinde uygulayabileceğimiz ufak bir proje. Gelecek güncellemeler ile GooglePlaystore' a yükleyebiliriz ama Basic hali bu.

---
+++ HitTheBall/app/src/main/java/com/melihakkose/hittheball/MainActivity.java
@@ -0,0 +1,118 @@
+package com.melihakkose.hittheball;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.Handler;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+    TextView textTime;
+    TextView textScore;
+    int score;
+    ImageView imageView;
+    ImageView imageView2;
+    ImageView imageView3;
+    ImageView imageView4;
+    ImageView imageView5;
+    ImageView imageView6;
+    ImageView imageView7;
+    ImageView imageView8;
+    ImageView imageView9;
+    ImageView[] imageArray;
+    Handler handler;
+    Runnable runnable;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Initialize
+        textTime=(TextView)findViewById(R.id.textTime);
+        textScore=(TextView)findViewById(R.id.textScore);
+        imageView=findViewById(R.id.imageView);
+        imageView2=findViewById(R.id.imageView2);
+        imageView3=findViewById(R.id.imageView3);
+        imageView4=findViewById(R.id.imageView4);
+        imageView5=findViewById(R.id.imageView5);
+        imageView6=findViewById(R.id.imageView6);
+        imageView7=findViewById(R.id.imageView7);
+        imageView8=findViewById(R.id.imageView8);
+        imageView9=findViewById(R.id.imageView9);
+        imageArray=new ImageView[]{imageView,imageView2,imageView3,imageView4,imageView5,imageView6,imageView7,imageView8,imageView9,};
+        hideImages();
+
+
+
+        score=0;
+
+        new CountDownTimer(10000,1000){
+
+            @Override
+            public void onTick(long l) {
+                textTime.setText("TIME: "+l/1000);
+            }
+
+            @Override
+            public void onFinish() {
+                textTime.setText("TIME OFF!");
+                handler.removeCallbacks(runnable);
+                for(ImageView image:imageArray){
+                    image.setVisibility(View.INVISIBLE);
+                }
+                AlertDialog.Builder alert=new AlertDialog.Builder(MainActivity.this);
+                alert.setTitle("Restart?");
+                alert.setMessage("Are you sure to restart game?");
+                alert.setPositiveButton("Yes!", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Intent intent =getIntent();
+                        finish();
+                        startActivity(intent);
+                    }
+                });
+                alert.setNegativeButton("No!", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(MainActivity.this,"Game Over!",Toast.LENGTH_LONG).show();
+                    }
+                });
+                alert.show();
+            }
+        }.start();
+
+    }
+    public void  increaseScore(View view){
+        score++;
+        textScore.setText("SCORE: "+score);
+
+    }
+
+    public void hideImages(){
+        handler =new Handler();
+        runnable =new Runnable() {
+            @Override
+            public void run() {
+                for(ImageView image:imageArray){
+                    image.setVisibility(View.INVISIBLE);
+                }
+                Random rand=new Random();
+                int number=rand.nextInt(9);
+                imageArray[number].setVisibility(View.VISIBLE);
+                handler.postDelayed(this,500);
+            }
+        };
+        handler.post(runnable);
+
+
+    }
+}
